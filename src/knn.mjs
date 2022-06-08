b9e59ee0fa23bd515f3b8187c13d2dc1b3790fed
shridharp <shridharpoman@gmail.com>
2022-06-08 00:51:11 +0000

    init

---
+++ src/knn.mjs
@@ -0,0 +1,14 @@
+import { ok, err } from 'cs544-js-utils';
+
+/** return pair [label, index] (wrapped within a Result) of the
+ *  training LabeledFeatures trainLabeledFeatures having the most
+ *  common label of the k training features closest to subject
+ *  testFeatures.
+ *
+ *  Errors:
+ *    BAD_FMT: trainLabeledFeatures has features bytes with length 
+ *             different from length of subject testFeatures.
+ */
+export default function  knn(testFeatures, trainLabeledFeatures, k=3) {
+  return err('knn() not implemented', { code: 'NO_IMPL' });
+}
---
+++ src/parse-images.mjs
@@ -0,0 +1,17 @@
+import { ok, err } from 'cs544-js-utils';
+
+/** parse byte streams in imageBytes: { images: Uint8Array, labels:
+ *  Uint8Array } as per imageSpecs { images: HeaderSpec[], labels:
+ *  HeaderSpec[] } to return a list of LabeledFeatures (wrapped within
+ *  a Result).
+ *
+ *  Errors:
+ *    BAD_VAL: value in byte stream does not match value specified
+ *             in spec.
+ *    BAD_FMT: size of bytes stream inconsistent with headers
+ *             or # of images not equal to # of labels.
+ */
+export default function parseImages(imageSpecs, imageBytes) {
+  return err('parseImages() not implemented', { code: 'NO_IMPL' });
+}
+
