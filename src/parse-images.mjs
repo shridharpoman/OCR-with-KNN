c2b444ac9113a4a600211e1dea2cc0e530d9f7b4
shridharp <shridharpoman@gmail.com>
2022-06-16 23:47:07 +0000

    Parse Images and Labels(W/O test cases)

--- src/parse-images.mjs
+++ src/parse-images.mjs
@@ -12,6 +12,37 @@ import { ok, err } from 'cs544-js-utils';
  *             or # of images not equal to # of labels.
  */
 export default function parseImages(imageSpecs, imageBytes) {
-  return err('parseImages() not implemented', { code: 'NO_IMPL' });
+
+
+  var data = new Uint8Array(imageBytes.images);
+  var dataView = new DataView(data.buffer);
+  var count = dataView.getInt32(4, false);
+
+
+  let n_rows = imageSpecs.images[2].value;
+  let n_cols = imageSpecs.images[3].value;
+
+  var pixelValues = [];
+
+  for (var image = 0; image <= count - 1; image++) {
+    var pixels = [];
+
+    for (var y = 0; y <= (n_cols - 1); y++) {
+      for (var x = 0; x <= (n_rows - 1); x++) {
+        pixels.push(imageBytes.images[(image * n_rows * n_rows) + (x + (y * n_rows)) + 16]);
+      }
+    }
+
+    var imageData = {};
+    // var label = imageBytes.labels[image + 8];
+    imageData["features"] = new Uint8Array(pixels);
+    imageData["label"] = JSON.stringify(imageBytes.labels[image + 8]);
+
+    pixelValues.push(imageData);
+  }
+
+  const Result = { val: pixelValues };
+
+  return Result;
 }
 
